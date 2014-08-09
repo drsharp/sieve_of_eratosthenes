b34cddd885a8b8d861f981a477da2e739e26f659
Dan Sharp <Dan.Sharp@irco.com>
2014-08-09 14:29:40 -0600

    Refactoring 17: Keep the Data Close to Where It Is Used
    Moved the guard clause to be in the sieve() method.

--- lib/primes.rb
+++ lib/primes.rb
@@ -1,18 +1,19 @@
 class Primes
 
   def initialize(max_value)
+    @max_value = max_value
     @is_prime = Array.new(&:false) # array of primes, false by default
     @is_prime_size = max_value + 1 # size of array of primes
     @is_prime_size.times {|i| @is_prime[i] = true }
   end
 
   def self.generate(max_value)
-    return [] if max_value < 2
     primes = Primes.new(max_value)
     return primes.sieve
   end
 
   def sieve
+    return [] if @max_value < 2
     result = Array.new
     (2..@is_prime_size).each do |i|
       if (@is_prime[i])
