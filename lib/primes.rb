8457dc3aed2b58a8566888f5c6a3b1d065aa24c7
Dan Sharp <Dan.Sharp@irco.com>
2014-08-09 12:47:19 -0600

    Refactoring 11: Replace Temp with Query (Actually Variable)
    We ended up with a well-named variable instead of a query because
    Ruby Arrays don't have a fixed length at declaration. This is still
    a good refactor because 's' was a meaningless variable name, but
    'is_prime_size' is a more meaningful name.

--- lib/primes.rb
+++ lib/primes.rb
@@ -5,20 +5,20 @@ class Primes
     return result if max_value < 2
 
     # declarations
-    s = max_value + 1 # size of array
-    is_prime = {}
-    s.times do |i|
+    is_prime = {} # array of primes
+    is_prime_size = max_value + 1 # size of array of primes
+    is_prime_size.times do |i|
       is_prime[i] = true
     end
 
     is_prime[0] = is_prime[1] = false
 
     # sieve
-    (2..s).each do |i|
+    (2..is_prime_size).each do |i|
       if (is_prime[i])
         result << i
         j = 2*i
-        while j<s do
+        while j < is_prime_size do
           is_prime[j] = false # multiple is not prime
           j += i
         end
